727b691792182cd3dbf632768f9feb1defb53ee6
evikary <daria_more10@mail.ru>
2024-03-26 17:04:43 +0300

    init: init project

---
+++ async-race/.eslintrc.js
@@ -0,0 +1,39 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'airbnb-base',
+    'airbnb-typescript/base',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+  ],
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: ['.eslintrc.{js,cjs}'],
+      parserOptions: {
+        sourceType: 'script',
+      },
+    },
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: './tsconfig.json',
+    tsconfigRootDir: __dirname,
+  },
+  plugins: ['import', '@typescript-eslint'],
+  rules: {
+    '@typescript-eslint/no-explicit-any': 'error',
+    'no-useless-escape': 'off',
+    '@typescript-eslint/no-use-before-define': 'off',
+    '@typescript-eslint/default-param-last': 'off',
+    'no-plusplus': 'off',
+  },
+};
